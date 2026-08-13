84bce55dc1e457a85aab07ed65d56b8171a8d4a5
Juan Francisco  Perdomo <juanperdomo918918@gmail.com>
2026-08-13 00:26:49 -0300

    fin

--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html')
+    context = {
+        'whatsapp_number': '5492216826109',
+    }
+    return render(request, 'index.html', context)
